15143dc400f1a929d3f11168c3cb28c31a1ccb31
seniorweasel <43583053+seniorweasel@users.noreply.github.com>
2020-04-29 16:14:53 +0300

    Add files via upload

---
+++ resultData.js
@@ -0,0 +1,14 @@
+
+// new Array("Sayfa Dosya Adı","Yol ","Sonuç Başlık","Anahtar kelimeler","Meta");
+
+
+c = 0; 
+item[c] = new Array("example.html", "bsodsolutions/", "Home", "index,main,start,home,front", "Start Here!");
+
+
+//DATA BURAYA EKLENECEK
+c++; item[c] = new Array("articles.html", "www.allmusic.com", "AllMusic | Record Reviews, Streaming Songs, Genres & Bands", "all,writing,article,write,precodes,code", "All the Best, Isaac Hayes · Pop/Rock, Comedy/Spoken. This collection of travel stories and observational anecdotes is completely spoken word backed by ...");
+c++; item[c] = new Array("playlists.html", "www.hepsiburada.com › ... › Monitör Bilgisayarlar", "Monitör PC (All-in-One) Fiyatları ve Çeşitleri Hepsiburada'da", "all,play,playlist,playlists,playing,songs,kavinsky", "En uygun ve kaliteli Bilgisayar Monitör çeşitleri Hepsiburada'da! PC All-in-One modellerini detaylarıyla incelemek ve satın almak için sayfayı ziyaret edin!");
+c++; item[c] = new Array("social.html", "wikipedia.com > All Elite Wrestling", "All Elite Wrestling - Wikipedia", "all,content,main,focus", "All Elite Wrestling, LLC (AEW) is an American professional wrestling promotion founded in 2019. Its inception was announced by entrepreneurs Shahid Khan ...");
+c++; item[c] = new Array("social.html", "photos.google.com", "Google Photos - All your photos organized and easy to find", "all,content,main,focus", "We've got your backup. Back up unlimited photos and videos for free, up to 16MP and 1080p HD. Access them from any phone, tablet, or computer on ...");
+c++; item[c] = new Array("social.html", "wikipedia.com > All Puşt Wrestling", "All Puşt Wrestling - Wikipedia", "all,content,main,focus", "All Elite Wrestling, LLC (AEW) is an American professional wrestling promotion founded in 2019. Its inception was announced by entrepreneurs Shahid Khan ...");
